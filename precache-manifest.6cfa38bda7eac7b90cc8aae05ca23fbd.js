e578593de11d65d0af57ae6680037acf10fac693
wilk6 <wilk6996@gmail.com>
2020-05-03 15:35:00 +0200

    Updates

---
+++ precache-manifest.6cfa38bda7eac7b90cc8aae05ca23fbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a9333688f243ea4285c1d206fa67cf",
+    "url": "/project01/index.html"
+  },
+  {
+    "revision": "dd05e8f76c5081475752",
+    "url": "/project01/static/css/main.9fc07ea8.chunk.css"
+  },
+  {
+    "revision": "b0c907a68630dd211cb2",
+    "url": "/project01/static/js/2.8e02ef67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project01/static/js/2.8e02ef67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd05e8f76c5081475752",
+    "url": "/project01/static/js/main.42c8c1d2.chunk.js"
+  },
+  {
+    "revision": "1bd407df3d3c0069c43e",
+    "url": "/project01/static/js/runtime-main.ee0456ed.js"
+  }
+]);
